67d7c7fbfed2ad9b29b43d659fb7896ed50c1324
zuBux <pariszoump@gmail.com>
2016-05-27 10:44:30 +0300

    Updated argument parsing. Handled common errors

--- cspgen.py
+++ cspgen.py
@@ -2,31 +2,38 @@ from cspgen import parser
 from cspgen import gen
 from cspgen import crawler
 import sys
+import logging
 import argparse
 
 
-def main(argv):
-    argparser = argparse.ArgumentParser(description='Content Security Policy generator')
-    argparser.add_argument('conf', nargs='?', help='TOML configuration file')
-    argparser.add_argument("-u", "--url", help="URL to crawl")
-    args = argparser.parse_args()
-
+def main(args):
     if args.url:
         profile = {}
         url = args.url
         page = crawler.get_page(url)
         profile['js_sources'], profile['inline'] = crawler.get_js_sources(page)
         pol = gen.policy_from_crawl(profile)
-        print pol
+        if args.output:
+            gen.write_toml(args.output, pol)
+        else:
+            print pol
     if args.conf:
-        for fname in args.conf:
-            conf = parser.parse_toml(fname)
-            if "scripts" in conf:
-                scrpts = parser.get_scripts_pol(conf)
-                script_pol = gen.scripts_pol(scrpts)
-                gen.print_policy()
+        try:
+            conf = parser.parse_toml(args.conf)
+        except Exception, e:
+            logging.error("Unable to open/read file %s: %s", fname,str(e))
+            return
+        if "scripts" in conf:
+            scrpts = parser.get_scripts_pol(conf)
+            script_pol = gen.scripts_pol(scrpts)
+            gen.print_policy()
     return
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    argparser = argparse.ArgumentParser(description='Content Security Policy generator')
+    argparser.add_argument('conf', nargs='?', type=str, help='TOML configuration file')
+    argparser.add_argument("-u", "--url", help="URL to crawl")
+    argparser.add_argument("-o", "--output", help="File to write CSP")
+    args = argparser.parse_args()
+    main(args)
